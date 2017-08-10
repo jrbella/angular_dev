7aa319113488a2767ff73a9ceaf8f762f38bd89e
jrbella <jbella@icstars.org>
2017-08-09 21:42:46 -0500

    added photoalbum modules

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,5 +10,10 @@ export class EntryComponent{
     title: string = 'My First Photo Album';
     photo: string = 'http://placehold.it/800x500?text=Angular Basics'
     description: string = 'A description of my first photo'
+    comments: any[] = [
+        {name: " Jeff", comment: " Hey my first comment!"},
+        {name: " Jeffrey", comment: " Hey my Second comment!"},
+        {name: " Jeffrey Bella", comment: " Hey my Third comment!"}
+    ]
 
 }
